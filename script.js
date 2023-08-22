4d8875e97759e3420504ea997101947be74acb9b
Aimee Gibson <aimjo617@gmail.com>
2023-08-22 09:25:50 -0600

    added carousel

--- script.js
+++ script.js
@@ -26,3 +26,32 @@ document.getElementById("glory-btn").addEventListener("click", function () {
     const quote = newQuote(gloryQuotes);
     document.getElementById('glory-quote-display').innerHTML = quote;
 });
+
+var slidePosition = 1;
+SlideShow(slidePosition);
+
+// forward/Back controls
+function plusSlides(n) {
+  SlideShow(slidePosition += n);
+}
+
+//  images controls
+function currentSlide(n) {
+  SlideShow(slidePosition = n);
+}
+
+function SlideShow(n) {
+  var i;
+  var slides = document.getElementsByClassName("container");
+  var circles = document.getElementsByClassName("dots");
+  if (n > slides.length) {slidePosition = 1}
+  if (n < 1) {slidePosition = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < circles.length; i++) {
+      circles[i].className = circles[i].className.replace(" enable", "");
+  }
+  slides[slidePosition-1].style.display = "block";
+  circles[slidePosition-1].className += " enable";
+} 
